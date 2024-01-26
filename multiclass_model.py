50de090dc83d672a78364803fd643fc63a1d6c77
yatchit888 <yatchit888@gmail.com>
2024-01-25 23:44:36 -0500

    Complete the hi_my_name_is function

--- multiclass_model.py
+++ multiclass_model.py
@@ -9,6 +9,7 @@ def define_dense_model_single_layer(input_length, activation_f='sigmoid', output
     activation_f: the activation function
     output_length: the number of outputs (number of neurons)"""
     model = keras.Sequential()
+    model.add(layers.Dense(output_length, activation=activation_f, input_shape=(input_length,)))
     return model
 
 def define_dense_model_with_hidden_layer(input_length, 
@@ -22,6 +23,8 @@ def define_dense_model_with_hidden_layer(input_length,
     output_length: the number of outputs (number of neurons in the output layer)"""
 
     model = keras.Sequential()
+    model.add(layers.Dense(hidden_layer_size, activation=activation_func_array[0], input_shape=(input_length,)))
+    model.add(layers.Dense(output_length, activation=activation_func_array[1]))
     return model
 
 
@@ -33,21 +36,29 @@ def get_mnist_data():
     x_test = x_test.reshape(10000, 784).astype('float32') / 255 
     return (x_train, y_train), (x_test, y_test)
 
-def fit_mnist_model(x_train, y_train, model, epochs=2, batch_size=2):
+from keras.utils import to_categorical
+
+def fit_mnist_model(x_train, y_train, model, epochs=10, batch_size=32):
     """Fit the model to the data.
     compile the model and add parameters for  the "optimizer", the loss function, 
     and the metrics, Hint: use categorical crossentropy for the loss function .
 
     then fit the model on the training data. (pass the epochs and batch_size params)
     """
-    # model.compile  ... 
-    # model.fit ...    
+    # One-hot encode the target data
+    y_train = to_categorical(y_train, num_classes=10)
+
+    model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+    model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size)
     return model
   
 def evaluate_mnist_model(x_test, y_test, model):
     """Evaluate the model on the test data.
     Hint: use model.evaluate() to evaluate the model on the test data.
     """
-    loss, accuracy = None, None # model evaluate
+    # One-hot encode the target data
+    y_test = to_categorical(y_test, num_classes=10)
+
+    loss, accuracy = model.evaluate(x_test, y_test)
     return loss, accuracy
 
